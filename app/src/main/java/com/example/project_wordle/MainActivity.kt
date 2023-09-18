d562b76df179bb7904554aed58d087587f67f4f3
Christopher Mejia <58187990+chrism1001@users.noreply.github.com>
2023-09-18 18:57:50 -0400

    created four letter word class to get a random word.

--- app/src/main/java/com/example/project_wordle/MainActivity.kt
+++ app/src/main/java/com/example/project_wordle/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.project_wordle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project_wordle.FourLetterWordList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun checkGuess(guess: String) : String {
+        var result = ""
+        for (i in 0..3) {
+            if (guess[i] == wordToGuess[i])
+                result += "0"
+            else if (guess[i] in wordToGuess)
+                result += "+"
+            else
+                result += "X"
+        }
+        return result
     }
 }
