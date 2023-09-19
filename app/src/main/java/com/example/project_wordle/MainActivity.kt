c0e6c90ca1aa72d2ebdccb018f0823be65625f63
Christopher Mejia <58187990+chrism1001@users.noreply.github.com>
2023-09-18 22:11:43 -0400

    added game functionality and guesses to keep track

--- app/src/main/java/com/example/project_wordle/MainActivity.kt
+++ app/src/main/java/com/example/project_wordle/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.project_wordle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.example.project_wordle.FourLetterWordList
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
-    val guesses = 3
+    var guesses = 3
+    var gameState = true
+    val word = FourLetterWordList.getRandomFourLetterWord()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +25,38 @@ class MainActivity : AppCompatActivity() {
         val answer1 = findViewById<TextView>(R.id.answerid1)
         val answer2 = findViewById<TextView>(R.id.answerid2)
         val answer3 = findViewById<TextView>(R.id.answerid3)
-        val input = findViewById<TextInputLayout>(R.id.textInputLayout1)
+        val input = findViewById<EditText>(R.id.textInputLayout1)
 
         button.setOnClickListener {
+            while (guesses >= 0 && gameState) {
+                if (input.text.toString() == word || guesses == 0) {
+                    gameState = false
+                    break
+                }
+                if (guesses == 3) {
+                    guess1.text = input.text.toString()
+                    answer1.text = checkGuess(guess1.text.toString(), word)
+                } else if (guesses == 2) {
+                    guess2.text = input.text.toString()
+                    answer2.text = checkGuess(guess2.text.toString(), word)
+                } else {
+                    guess3.text = input.text.toString()
+                    answer3.text = checkGuess(guess3.text.toString(), word)
+                }
 
+                input.text.clear()
+                guesses--
+            }
+
+            if (!gameState) {
+                Toast.makeText(it.context, "You Won!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(it.context, "You Lost!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
-    private fun checkGuess(guess: String) : String {
+    private fun checkGuess(guess: String, wordToGuess: String) : String {
         var result = ""
         for (i in 0..3) {
             if (guess[i] == wordToGuess[i])
